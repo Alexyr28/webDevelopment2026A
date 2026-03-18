e25f6a5e1375559392235b3e556fff26dc499d64
Alexyr28 <alexyr24@gmail.com>
2026-03-18 08:54:33 -0500

    feat: React_Clase1

---
+++ minipos-web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola Santiago Cacorro</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam perspiciatis culpa, animi id placeat laboriosam ut voluptates nisi cupiditate quaerat magni labore at ipsam, non harum nihil nam ducimus veniam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, ab laborum. Totam deserunt fugit labore incidunt nesciunt excepturi commodi at velit cum quod! Laboriosam, minima id eum fuga quia similique.</p>
+      <input type="button" value="Coscorrea"/>
+    </>
+  )
+}
+
+export default App
