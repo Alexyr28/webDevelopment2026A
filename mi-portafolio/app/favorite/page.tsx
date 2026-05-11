d67ca293fba2f6e7a5c89b3c7237a97bda3f0dd4
Alexyr28 <alexyr24@gmail.com>
2026-05-11 12:42:56 -0500

    feat: Fullstack

---
+++ mi-portafolio/app/favorite/page.tsx
@@ -0,0 +1,10 @@
+export default function FavoritePage() {
+    return (
+        <main>
+            <h1>Favorito</h1>
+            <p>
+                Sección de favoritos
+            </p>
+        </main>
+    );
+}
---
+++ mi-portafolio/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hola, soy Jorge</h1>
+      <p>Bienvenido a mi portafolio construido con Next.js.</p>
+    </main>
+  );
+}
